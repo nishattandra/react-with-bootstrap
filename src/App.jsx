9fff4757e6c199f2c2ca73f8506d55318e4deb41
nishattandra <nishattandra2001@gmail.com>
2023-04-02 17:24:42 +0600

    first system using bootstrap in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +29,43 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+      <button type="button" className="btn btn-link">Link</button>
+
+      <div className="card-group">
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
